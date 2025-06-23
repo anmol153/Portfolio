b8864e67e8a1122d61da02645acadf424225549d
AnmolDhima12 <anmoldhimans525@gmail.com>
2025-06-23 18:19:51 +0530

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,6 @@ function App() {
   return (
     <>
       <main className='h-full w-full'>
-        <div className='flex flex-col gap-20 h-[850px]'>
-          <Hero/>
-        </div>
         <Navbar/>
       </main>
     </>
